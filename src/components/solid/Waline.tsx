b0aa8223e6980720b17cd69aacf152a64258482a
bqxbqx <boqingxin14@gmail.com>
2024-05-07 12:27:59 +0800

    feat: comment section

--- src/components/solid/Waline.tsx
+++ src/components/solid/Waline.tsx
@@ -17,7 +17,7 @@ export const Waline = (props: WalineOptions) => {
     walineRef = init({
       ...props,
       el: containerRef,
-      serverURL: "http://localhost:8360/",
+      serverURL: "https://comment.6qx.top/",
       emoji: [
         "//unpkg.com/@waline/emojis@1.2.0/tw-emoji",
         "//unpkg.com/@waline/emojis@1.2.0/tieba",
