cd41bf65bb6bbf6fd4c1af0dc08a08d00d580cc8
bqxbqx <boqingxin14@gmail.com>
2024-06-05 12:15:32 +0800

    feat: header color

--- src/components/solid/Header.tsx
+++ src/components/solid/Header.tsx
@@ -8,63 +8,55 @@ interface HeaderProps {
 }
 
 export default function Header(props: HeaderProps) {
-  const [isPhone, setIsPhone] = createSignal<boolean | undefined>(undefined);
   const [isShowOptions, setIsShowOptions] = createSignal<boolean>(false);
 
-  onMount(() => {
-    const userAgent = window.navigator.userAgent;
-    const isMobile = /iPhone|iPad|iPod|Android/i.test(userAgent);
-    setIsPhone(isMobile);
-  });
-
   return (
-    <header
-      class={`${styles["header-container"]} ${
-        isShowOptions() ? styles["show-options"] : ""
-      }`}
-      style={{ position: `${props.type}` }}
-    >
-      <div class={`${styles["header-nav-container"]}`}>
-        <a href="/">
-          <span class={styles["site-title"]}>{SITE_TITLE}</span>
-        </a>
-        {!isPhone() ? (
-          <div class={styles["header-link-container"]}>
-            <HeaderLink href="/">博客们。</HeaderLink>
-            <HeaderLink href="/about">关于我。</HeaderLink>
+    <>
+        <header
+          class={`${styles["header-container"]} ${
+            isShowOptions() ? styles["show-options"] : ""
+          }`}
+          style={{ position: `${props.type}` }}
+        >
+          <div class={`${styles["header-nav-container"]}`}>
+            <a href="/">
+              <span class={styles["site-title"]}>{SITE_TITLE}</span>
+            </a>
+            <div class={styles["header-link-container"]}>
+              <HeaderLink href="/">博客们。</HeaderLink>
+              <HeaderLink href="/about">关于我。</HeaderLink>
+            </div>
+            <div
+              class={styles["more-svg-container"]}
+              onclick={() => setIsShowOptions(!isShowOptions())}
+            >
+              <div
+                class={`${styles.top} ${
+                  isShowOptions() ? styles["top-clicked"] : ""
+                }`}
+              />
+              <div
+                class={`${styles.middle} ${
+                  isShowOptions() ? styles["middle-clicked"] : ""
+                }`}
+              />
+              <div
+                class={`${styles.bottom} ${
+                  isShowOptions() ? styles["bottom-clicked"] : ""
+                }`}
+              />
+            </div>
           </div>
-        ) : (
-          <div
-            class={styles["more-svg-container"]}
-            onclick={() => setIsShowOptions(!isShowOptions())}
-          >
-            <div
-              class={`${styles.top} ${
-                isShowOptions() ? styles["top-clicked"] : ""
-              }`}
-            />
-            <div
-              class={`${styles.middle} ${
-                isShowOptions() ? styles["middle-clicked"] : ""
-              }`}
-            />
-            <div
-              class={`${styles.bottom} ${
-                isShowOptions() ? styles["bottom-clicked"] : ""
-              }`}
-            />
+          <div class={styles["options-container"]}>
+            <div class={styles["link-item"]}>
+              <HeaderLink href="/">博客们。</HeaderLink>
+            </div>
+            <div class={styles["link-item"]}>
+              <HeaderLink href="/about">关于我。</HeaderLink>
+            </div>
+            <div class={styles.divider}></div>
           </div>
-        )}
-      </div>
-      <div class={styles["options-container"]}>
-        <div class={styles["link-item"]}>
-          <HeaderLink href="/">博客们。</HeaderLink>
-        </div>
-        <div class={styles["link-item"]}>
-          <HeaderLink href="/about">关于我。</HeaderLink>
-        </div>
-        <div class={styles.divider}></div>
-      </div>
-    </header>
+        </header>
+    </>
   );
 }
